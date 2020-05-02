06606b87f04aa94878c2dce984b08f17c2af7fd3
Maxime Mérian <maxime@merian.me>
2020-05-02 12:59:24 +0200

    Add pre-exec script

--- start-dovecot.sh
+++ start-dovecot.sh
@@ -9,4 +9,11 @@ if [ ! "$(ls -A /etc/dovecot)" ]; then
     echo 'log_path = /dev/stdout' >> /etc/dovecot/conf.d/10-logging.conf
 fi
 
+if [ ! -z "$PRE_EXEC"  ] && [ -x "$PRE_EXEC" ]; then
+    echo "Executing pre-exec $PRE_EXEC"
+    $PRE_EXEC
+fi
+
+# Start dovecot
+echo "Starting dovecot"
 /usr/sbin/dovecot -F -c /etc/dovecot/dovecot.conf
